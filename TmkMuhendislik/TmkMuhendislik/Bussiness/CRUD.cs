eef0576d46d1366a7d04994baf82af548c5753e4
Ali Biner <102030146+AliBiner@users.noreply.github.com>
2023-07-15 01:07:52 +0300

    NewsPage Read Process

---
+++ TmkMuhendislik/TmkMuhendislik/Bussiness/CRUD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TmkMuhendislik.Models;
+
+namespace TmkMuhendislik.Bussiness
+{
+    public class CRUD
+    {
+        private TmkMuhendislikEntities db = new TmkMuhendislikEntities();
+        public List<News> Control()
+        {
+            var controls = db.News.Where(x=>x.N_Check==true).ToList();
+            if (controls==null)
+            {
+                return null;
+            }
+            else
+            {
+                
+                return controls;
+            }
+        }
+
+    }
+}
--- TmkMuhendislik/TmkMuhendislik/Controllers/NewsController.cs
+++ TmkMuhendislik/TmkMuhendislik/Controllers/NewsController.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TmkMuhendislik.Bussiness;
 
 namespace TmkMuhendislik.Controllers
 {
+    
     public class NewsController : Controller
     {
+        private CRUD c = new CRUD();
         // GET: News
         public ActionResult Index()
         {
-            return View();
+            var model = c.Control();
+
+            return View(model);
         }
 
         public ActionResult Detail()
         {
             return View();
         }
+
+
     }
 }
